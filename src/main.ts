f183bfae3801f6dc97bdd41a70d141d44131bc1b
Mitchell Vivian <mitchell.j.vivian@gmail.com>
2025-03-26 14:29:40 -0700

    Remove unused template code

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,20 @@
 import { AggregateDailyNotesCommand } from './commands/aggregate-daily-notes';
 import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface NoteAggregatorSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: NoteAggregatorSettings = {
 	mySetting: 'default'
 }
 
 export default class DailyNoteAggregatorPlugin extends Plugin {
-	settings!: MyPluginSettings;
+	settings!: NoteAggregatorSettings;
 	internalDateFormat = 'YYYY-MM-DD';
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
@@ -32,49 +22,8 @@ export default class DailyNoteAggregatorPlugin extends Plugin {
 
 		this.addCommand(new AggregateDailyNotesCommand(this));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		/*
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-		*/
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		/*
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-		*/
+		this.addSettingTab(new NoteAggregatorSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -90,23 +39,7 @@ export default class DailyNoteAggregatorPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class NoteAggregatorSettingTab extends PluginSettingTab {
 	plugin: DailyNoteAggregatorPlugin;
 
 	constructor(app: App, plugin: DailyNoteAggregatorPlugin) {
